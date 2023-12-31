ecef11f78e91a53e8f49b797bc06614bbb5a45bd
edlevco <Edlevco88@gmail.com>
2023-12-30 23:16:02 -0300

    Added more contries and improved UX
    Firstly, if the user is stuck, they can type ? to get one letter unlocked. This will cost them 3 guesses rather than 1 per guess. In addition a guess is added for every guess even if the letter is in the country. The guess will not be added if the country is unlocked.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
-countries = [["canada", "mexico", "argentina"],
-             ["spain", "portugal", "sweden"],
-             ["mongolia", "china", "russia"]]
-levelInput = (int(input("What level do you want to play (1-3)"))) - 1
+countries = [["canada", "mexico", "argentina", "united states", "india"],
+             ["spain", "portugal", "sweden", "poland", "germany"],
+             ["mongolia", "china", "russia", "chile", "peru"]]
+print("Welcome to the countries hang-man game!")
+print("If you are stuck type '?' but it will cost 3 guesses")
+levelInput = (int(input("What level do you want to play (1-3): "))) - 1
 chosen_country = random.choice(countries[levelInput])
 display = []
 for letters in chosen_country:
@@ -19,6 +21,13 @@ while wordUnknown:
     print(f"{' '.join(display)}")
     print(f"Guesses: {guesses}")
     letterGuess = input("Guess a letter or country: ")
+    if letterGuess == "?":
+        guesses += 3
+        while True:
+            randNum = random.randint(0, len(chosen_country) - 1)
+            if not chosen_country[randNum] == display[randNum]:
+                display[randNum] = chosen_country[randNum]
+                break
     for positions in range(len(chosen_country)):
         if chosen_country[positions] == letterGuess:
             display[positions] = letterGuess
@@ -27,5 +36,4 @@ while wordUnknown:
         print(f"You got it correct, the country was {chosen_country}")
         print(f"It took you {guesses} guesses")
         wordUnknown = False
-    if letterGuess not in chosen_country:
-        guesses += 1
+    guesses += 1
